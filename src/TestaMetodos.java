f5bf5cfbd5b8df3a8bef7657fa9b2489c72fb507
Caitano <josiasmartinscaitano@gmail.com>
2023-01-08 14:13:42 -0300

    feat: definindo comportamentos

---
+++ src/TestaMetodos.java
@@ -0,0 +1,25 @@
+public class TestaMetodos {
+
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        conta.saldo = 100;
+        conta.deposita(50);
+
+        System.out.println(conta.saldo);
+        boolean conseguiuRetirar = conta.saca(20);
+        System.out.println(conta.saldo);
+        System.out.println(conseguiuRetirar);
+
+        Conta contaDaMaria = new Conta();
+        contaDaMaria.deposita(1000);
+
+        boolean sucessoTransferencia = contaDaMaria.transfere(3000, conta);
+        if (sucessoTransferencia) {
+            System.out.println("transferência com sucesso");
+        } else {
+            System.out.println("faltou dinheiro");
+        }
+        System.out.println(contaDaMaria.saldo);
+    }
+
+}
